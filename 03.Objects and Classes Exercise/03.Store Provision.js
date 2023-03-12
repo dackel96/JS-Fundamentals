01d7dcaaa485f8c6ceb63126c6db9d1a816933c2
Ivan Iliev <ivanilievdackel@gmail.com>
2023-03-12 14:17:26 +0200

    Update 03.Store Provision.js

--- 03.Objects and Classes Exercise/03.Store Provision.js
+++ 03.Objects and Classes Exercise/03.Store Provision.js
@@ -22,3 +22,28 @@ function stocksStore(stock, order) {
     console.log(`${key} -> ${store[key]}`);
   }
 }
+
+function secondSolve(stock, order) {
+  let combined = [...stock, ...order];
+
+  let store = combined.reduce((data, currValue, index) => {
+    let product = currValue;
+
+    if (i % 2 === 0) {
+      if (!store.hasOwnProperty(product)) {
+        store[product] = 0;
+      }
+    } else {
+      let value = Number(product);
+
+      let previous = combined[i - 1];
+
+      store[previous] += value;
+    }
+    return data;
+  }, {});
+
+  for (const key in store) {
+    console.log(`${key} -> ${store[key]}`);
+  }
+}
