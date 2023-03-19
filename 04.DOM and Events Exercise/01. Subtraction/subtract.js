ffaf705f752fefbe25057091d6da68faed78ff0a
Ivan Iliev <ivanilievdackel@gmail.com>
2023-03-19 20:31:05 +0200

    calc in dom

--- 04.DOM and Events Exercise/01. Subtraction/subtract.js
+++ 04.DOM and Events Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,15 @@
 function subtract() {
-    console.log('TODO:...');
+  const firstNum = Number(document.querySelector('#firstNumber').value);
+
+  console.log(firstNum);
+
+  const secondNum = Number(document.querySelector('#secondNumber').value);
+
+  console.log(secondNum);
+
+  const result = document.querySelector('#result');
+
+  result.textContent = Number(firstNum - secondNum);
+
+  console.log(result.textContent);
 }
