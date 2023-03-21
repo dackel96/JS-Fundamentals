bbc92b9c05e525597adf15cd43b7d2ebfd9cbe34
Ivan Iliev <ivanilievdackel@gmail.com>
2023-03-21 17:44:16 +0200

    Update solution.js

--- 04.DOM and Events Exercise/08. Furniture/solution.js
+++ 04.DOM and Events Exercise/08. Furniture/solution.js
@@ -1,4 +1,31 @@
 function solve() {
+  function createElement(type, content, id, parentNode, classes, attributes) {
+    const newElement = document.createElement(type);
 
-  //TODO...
+    if (content && type !== 'input') {
+      newElement.textContent = content;
+    }
+
+    if (content && type === 'input') {
+      newElement.value = content;
+    }
+
+    if (id) {
+      newElement.id = id;
+    }
+
+    if (parentNode) {
+      parentNode.appendChild(newElement);
+    }
+
+    if (classes) {
+      newElement.classList.add(...classes);
+    }
+
+    if (attributes) {
+      for (const key in attributes) {
+        newElement.setAttribute(key, attributes[key]);
+      }
+    }
+  }
 }
