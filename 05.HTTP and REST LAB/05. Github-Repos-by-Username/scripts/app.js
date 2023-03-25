dfb15932765c63b93dcc0a431704752d96be2cac
Ivan Iliev <ivanilievdackel@gmail.com>
2023-03-26 01:10:36 +0200

    gitHubApiPlayground

--- 05.HTTP and REST LAB/05. Github-Repos-by-Username/scripts/app.js
+++ 05.HTTP and REST LAB/05. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,45 @@
 function loadRepos() {
-	console.log("TODO...");
+  const BASE_URL = 'https://api.github.com/users/';
+
+  const input = document.getElementById('username');
+
+  const username = input.value;
+
+  const fullUrl = `${BASE_URL}${username}/repos`;
+
+  const body = document.getElementById('bdy');
+
+  let listResult = document.getElementById('repos');
+
+  input.addEventListener('focus', inputClear);
+
+  fetch(fullUrl, { method: 'GET' })
+    .then((result) => result.json())
+    .then((data) => {
+      data.forEach((element) => {
+        if (listResult === null) {
+          listResult = document.createElement('ul');
+          body.appendChild(listResult);
+        }
+        let newLi = document.createElement('li');
+
+        let newA = document.createElement('a');
+
+        newA.textContent = element.full_name;
+
+        const link = element.html_url.toString();
+
+        newA.setAttribute('href', link);
+
+        newLi.appendChild(newA);
+
+        listResult.appendChild(newLi);
+
+        input.value = '';
+      });
+    });
+
+  function inputClear() {
+    listResult.remove();
+  }
 }
--- 05.HTTP and REST LAB/06. Github-Commits/app.js
+++ 05.HTTP and REST LAB/06. Github-Commits/app.js
@@ -1,4 +1,89 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+  const BASE_URL = 'https://api.github.com/repos/';
+
+  const userInput = document.getElementById('username');
+
+  const repoInput = document.getElementById('repo');
+
+  const body = document.getElementById('bdy');
+
+  let listResult = document.getElementById('commits');
+
+  const username = userInput.value;
+
+  const repo = repoInput.value;
+
+  const fullLink = `${BASE_URL}${username}/${repo}/commits`;
+
+  repoInput.addEventListener('focus', clearList);
+  userInput.addEventListener('focus', clearList);
+
+  fetch(fullLink, { method: 'GET' })
+    .then((x) => x.json())
+    .then((data) => {
+      data.forEach(({ commit }) => {
+        if (listResult === null) {
+          listResult = document.createElement('ul');
+          body.appendChild(listResult);
+        }
+        const newLi = document.createElement('li');
+
+        newLi.textContent = `${commit.author.name}: ${commit.message}`;
+
+        listResult.appendChild(newLi);
+
+        repoInput.value = '';
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  function clearList() {
+    listResult.remove();
+  }
+}
+
+function loadRepos() {
+  const BASE_URL = 'https://api.github.com/users/';
+
+  const input = document.getElementById('username');
+
+  const username = input.value;
+
+  const fullUrl = `${BASE_URL}${username}/repos`;
+
+  const body = document.getElementById('bdy');
+
+  let listResult = document.getElementById('repos');
+
+  input.addEventListener('focus', inputClear);
+
+  fetch(fullUrl, { method: 'GET' })
+    .then((result) => result.json())
+    .then((data) => {
+      data.forEach((element) => {
+        if (listResult === null) {
+          listResult = document.createElement('ul');
+          body.appendChild(listResult);
+        }
+        let newLi = document.createElement('li');
+
+        let newA = document.createElement('a');
+
+        newA.textContent = element.full_name;
+
+        const link = element.html_url.toString();
+
+        newA.setAttribute('href', link);
+
+        newLi.appendChild(newA);
+
+        listResult.appendChild(newLi);
+      });
+    });
+
+  function inputClear() {
+    listResult.remove();
+  }
 }
