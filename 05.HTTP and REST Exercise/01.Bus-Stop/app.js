7031f507e01c3058a2ddfed987d7fec0e0f9f897
Ivan Iliev <ivanilievdackel@gmail.com>
2023-03-27 01:47:44 +0300

    GET method with fetch

--- 05.HTTP and REST Exercise/01.Bus-Stop/app.js
+++ 05.HTTP and REST Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,32 @@
 function getInfo() {
-    // TODO:
+  const BASE_URL = 'http://localhost:3030/jsonstore/bus/businfo/';
+
+  const input = document.getElementById('stopId');
+
+  const fullLink = `${BASE_URL}${input.value}`;
+
+  const stopName = document.getElementById('stopName');
+
+  const resultContainer = document.getElementById('buses');
+
+  resultContainer.innerHTML = '';
+
+  fetch(fullLink)
+    .then((x) => x.json())
+    .then((data) => {
+      const { name, buses } = data;
+
+      stopName.textContent = name;
+
+      for (const bus in buses) {
+        const newLi = document.createElement('li');
+
+        newLi.textContent = `Bus ${bus} arrives in ${buses[bus]} minutes`;
+
+        resultContainer.appendChild(newLi);
+      }
+    })
+    .catch((err) => {
+      stopName.textContent = 'Error';
+    });
 }
--- 05.HTTP and REST Exercise/02.Bus-Schedule/app.js
+++ 05.HTTP and REST Exercise/02.Bus-Schedule/app.js
@@ -1,16 +1,48 @@
 function solve() {
-    function depart() {
-        // TODO: 
-    }
+  const BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/';
 
-    async function arrive() {
-        // TODO:
-    }
+  let id = 'depot';
 
-    return {
-        depart,
-        arrive
-    };
+  const infoContainer = document.querySelector('#info .info');
+
+  const arriveBtn = document.getElementById('arrive');
+
+  const departBtn = document.getElementById('depart');
+
+  function depart() {
+    fetch(`${BASE_URL}${id}`, { method: 'GET' })
+      .then((x) => x.json())
+      .then((data) => {
+        const { name, next } = data;
+
+        infoContainer.textContent = `Next stop ${name}`;
+
+        departBtn.disabled = true;
+
+        arriveBtn.disabled = false;
+      });
+  }
+
+  async function arrive() {
+    fetch(`${BASE_URL}${id}`, { method: 'GET' })
+      .then((x) => x.json())
+      .then((data) => {
+        const { name, next } = data;
+
+        infoContainer.textContent = `Arriving at ${name}`;
+
+        arriveBtn.disabled = true;
+
+        departBtn.disabled = false;
+
+        id = next.toString();
+      });
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
