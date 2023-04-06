f4cbe99a25b58849fa100249f381a3d7c08eebee
Ivan Iliev <ivanilievdackel@gmail.com>
2023-04-06 19:52:02 +0300

    06,07

--- 05.HTTP and REST Exercise/05.Messenger/app.js
+++ 05.HTTP and REST Exercise/05.Messenger/app.js
@@ -1,5 +1,54 @@
 function attachEvents() {
-    // TODO:
+  const BASE_URL = 'http://localhost:3030/jsonstore/messenger';
+  const authorInputElement = Array.from(
+    document.querySelectorAll('#controls > div > input')
+  )[0];
+
+  const contentInputElement = Array.from(
+    document.querySelectorAll('#controls > div > input')
+  )[1];
+
+  const submitBtn = document.getElementById('submit');
+
+  const refreshBtn = document.getElementById('refresh');
+
+  const resultContainer = document.getElementById('messages');
+
+  submitBtn.addEventListener('click', submitForm);
+
+  refreshBtn.addEventListener('click', refreshMessageBox);
+
+  function submitForm() {
+    let authorName = authorInputElement.value;
+
+    let contentText = contentInputElement.value;
+
+    const httpHeader = {
+      method: 'POST',
+      body: JSON.stringify({ author: authorName, content: contentText }),
+    };
+
+    fetch(BASE_URL, httpHeader);
+
+    authorInputElement.value = '';
+
+    contentInputElement.value = '';
+  }
+
+  function refreshMessageBox() {
+    resultContainer.textContent = '';
+
+    let result = '';
+
+    fetch(BASE_URL)
+      .then((x) => x.json())
+      .then((data) => {
+        for (const key in data) {
+          result += `${data[key].author}: ${data[key].content}\n`;
+        }
+        resultContainer.textContent = result.trimEnd();
+      });
+  }
 }
 
 attachEvents();
--- 05.HTTP and REST Exercise/06.Phonebook/app.js
+++ 05.HTTP and REST Exercise/06.Phonebook/app.js
@@ -1,5 +1,69 @@
 function attachEvents() {
-    // TODO:
-}
+  const BASE_URL = 'http://localhost:3030/jsonstore/phonebook';
 
+  let id;
+
+  const createBtn = document.getElementById('btnCreate');
+
+  const loadBtn = document.getElementById('btnLoad');
+
+  const personInputElement = document.getElementById('person');
+
+  const phoneInputElement = document.getElementById('phone');
+
+  const resultContainer = document.getElementById('phonebook');
+
+  createBtn.addEventListener('click', createNewData);
+
+  loadBtn.addEventListener('click', loadFromServer);
+
+  function createNewData() {
+    let httpHeader = {
+      method: 'POST',
+      body: JSON.stringify({
+        person: personInputElement.value,
+        phone: phoneInputElement.value,
+      }),
+    };
+
+    fetch(BASE_URL, httpHeader).then((x) => x.json());
+
+    loadFromServer();
+
+    personInputElement.value = '';
+    phoneInputElement.value = '';
+  }
+
+  function loadFromServer() {
+    fetch(BASE_URL)
+      .then((x) => x.json())
+      .then((data) => {
+        resultContainer.innerHTML = '';
+        for (const key in data) {
+          const newLi = document.createElement('li');
+          const deleteBtn = document.createElement('button');
+          deleteBtn.textContent = 'Delete';
+
+          deleteBtn.id = data[key]._id;
+
+          newLi.textContent = `${data[key].person}: ${data[key].phone}`;
+
+          newLi.appendChild(deleteBtn);
+          resultContainer.appendChild(newLi);
+
+          deleteBtn.addEventListener('click', deleteEntity);
+        }
+      });
+
+    function deleteEntity() {
+      const entityId = this.id;
+
+      fetch(`${BASE_URL}/${entityId}`, { method: 'DELETE' })
+        .then((x) => x.json())
+        .then((data) => {
+          loadFromServer();
+        });
+    }
+  }
+}
 attachEvents();
